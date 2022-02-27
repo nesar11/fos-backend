b09d55f593af4b729f69365893b085ad10ab3863
nesar11 <nesar.uddin100@gmail.com>
2022-02-27 10:40:05 +0800

    MongoDB connection

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const app = express();
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
+const mongoose = require('mongoose');
+const config = require('./app/config/DB');
 const PORT = process.env.PORT|| 3000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
 
 
 app.use(express.static('public'));
@@ -15,27 +26,9 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'))
 
 app.set('view engine', 'ejs');
+require('./routes/web')(app)
 
-app.get('/', (req, res) => {
 
-    res.render('home')
-  
-});
-app.get('/cart', (req, res) => {
-
-       res.render('customers/cart')
-
-});
-app.get('/login', (req, res) => {
-
-       res.render('auth/login')
-
-});
-app.get('/register', (req, res) => {
-
-       res.render('auth/register')
-
-});
 
 app.listen(PORT, () =>{
     console.log(`listening on port ${PORT}`);
