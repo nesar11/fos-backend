9929dd2ddf8cd2426fa569238fdb6c538561d591
nesar11 <nesar.uddin100@gmail.com>
2022-02-28 20:47:20 +0800

    session store to DB

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
-const PORT = process.env.PORT|| 3000;
+const PORT = process.env.PORT|| 3004;
 const mongoose = require('mongoose');
-const config = require('./app/config/DB');
+const settings = require('./app/config/DB');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+// var app = express();
+
+
 
 // database connection
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, {
+mongoose.connect(settings.DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true
   }).then(
@@ -17,6 +25,31 @@ mongoose.connect(config.DB, {
   err => { console.log('Can not connect to the database'+ err)}
 );
 
+
+var store = new MongoDBStore({
+  uri: process.env.MONGO_CONNECTION_UR,
+  // uri: 'mongodb://localhost:27017/fos_db',
+  collection: 'mySessions'
+});
+
+// Catch errors
+store.on('error', function(error) {
+  console.log(error);
+});
+
+
+app.use(require('express-session')({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  store: store,
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+app.use(flash());
 // front end direction
 app.use(express.static('public'));
 
