4b9d9080b3a1de8c5f61d820cfe514214e817870
nesar11 <nesar.uddin100@gmail.com>
2022-03-04 21:19:12 +0800

    register api

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
@@ -41,11 +46,15 @@ app.use(session({
   cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
 }))
 
-
+app.use(bodyParser.json());
+app.use(cors());
 app.use(flash());
 // front end direction
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}))
+
 app.use(express.json());
+
 app.use((req, res, next) => {
   res.locals.session = req.session
   next()
@@ -60,6 +69,9 @@ app.set('views', path.join(__dirname, '/resources/views'))
 
 app.set('view engine', 'ejs');
 require('./routes/web')(app)
+app.use((req, res) => {
+    res.status(404).render('errors/404')
+})
 
 
 
